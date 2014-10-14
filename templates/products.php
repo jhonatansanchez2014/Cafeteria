01953f8ca6c493e5a8419e91b23a88ed73a3f873
Pedro Hernández Villegas <design.pweb@gmail.com>
2014-10-14 01:47:53 -0500

    update

--- templates/products.php
+++ templates/products.php
@@ -144,15 +144,15 @@
 								<option value="Gaseosa">Gaseosa</option>
 							</select>-->
 
-							<input style=" display: inline-block; width: 150px;" name="cantidad" type="number" min="1" max="255" onkeypress="validatenum();" autocomplete="off" maxlength="10" class="form-control" placeholder="Cantidad" required />
+							<input style=" display: inline-block; width: 158px;" name="cantidad" type="number" min="1" max="255" onkeypress="validatenum();" autocomplete="off" maxlength="10" class="form-control" placeholder="Cantidad" required />
 
-							<select  style="display: inline-block; width: 152px;" name="medida" class="form-control salto">
+							<select  style="display: inline-block; width: 200px;" name="medida" class="form-control salto">
 								<option value="Unidad">Unidad</option>
 								<option value="Kg">Kilogramos</option>
 								<option value="Lb">Libras</option>
 							</select>
 							
-							<input style=" display: inline-block; width: 150px;" name="precio" type="text" maxlength="255" onkeypress="validatenum();" autocomplete="off" class="form-control salto" placeholder="Precio" required />
+							<input style=" display: inline-block; width: 200px;" name="precio" type="text" maxlength="255" onkeypress="validatenum();" autocomplete="off" class="form-control salto" placeholder="Precio" required />
 
 							<label class="salto">Fecha de caducidad</label>
 							<input name="fVence" type="date" autocomplete="off" class="form-control salto" placeholder="Fecha de vencimiento" required />
