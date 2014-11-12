63efa76ed7381f58810540f384102224f565e910
Pedro Hernández Villegas <design.pweb@gmail.com>
2014-11-12 00:51:33 -0500

    [Update]

--- includes/change.password.php
+++ includes/change.password.php
@@ -16,14 +16,22 @@
     $rpasswordnew = mysqli_real_escape_string($sqli, $_POST['rpass']);//Recibo por POST
 
     //Se comparan los password para verificar que sean iguales
-    if(($passwordnew == $rpasswordnew) && ($passwordold == $password)){
+    if($passwordnew == $rpasswordnew && $passwordold == $password){
     	//$sql = "UPDATE login SET password ='$passwordnew' WHERE password = '$user'";
 
-    	$mensaje = "Contraseña cambiado con éxito";
+    	$mensaje = '<div class="alert alert-warning alert-dismissible" role="alert">
+                		<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                		Contraseña cambiado con éxito, tu sesión será finalizda para que inicies nuevamente.
+            		</div>
+            	';
     	$error = true;
     }
     else{
-    	$mensaje = "Las contraseñas ingresadoas no coinciden, por favor verificalas e intenta de nuevo";
+    	$mensaje = '<div class="alert alert-warning alert-dismissible" role="alert">
+                		<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                		Las contraseñas ingresadoas no coinciden, por favor verificalas e intenta de nuevo.
+            		</div>
+            	';
     	$error = false;
     }
 
